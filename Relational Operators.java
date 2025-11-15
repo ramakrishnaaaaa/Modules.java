5a0c1c8242b18bac5ff37e5e813e4e8086c47f7e
ramakrishnaaaaa <ramakrishnaa399@gmail.com>
2025-11-15 22:42:07 +0530

    Add CompareNumbers class for number comparison

---
+++ Relational Operators.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        // Comparison
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        } 
+        else if (b > a) {
+            System.out.println(b + " is greater than " + a);
+        } 
+        else {
+            System.out.println("Both numbers are equal.");
+        }
+
+        sc.close();
+    }
+}
