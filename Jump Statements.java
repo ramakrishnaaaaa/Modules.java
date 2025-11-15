06817a2ff92055c48b71b97af4a6367beff1f9a3
ramakrishnaaaaa <ramakrishnaa399@gmail.com>
2025-11-15 22:46:21 +0530

    Create Jump Statements.java

---
+++ Jump Statements.java
@@ -0,0 +1,13 @@
+public class SkipNumber {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 5) {
+                continue;  // Skip number 5
+            }
+
+            System.out.println(i);
+        }
+    }
+}
