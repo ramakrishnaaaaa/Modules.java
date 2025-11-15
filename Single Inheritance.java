6f1eaf550b553c5e547ca15fbb27c3a1a29ec326
ramakrishnaaaaa <ramakrishnaa399@gmail.com>
2025-11-15 22:47:51 +0530

    Create Single Inheritance.java

---
+++ Single Inheritance.java
@@ -0,0 +1,20 @@
+ public class SingleInheritanceExample {
+    public static void main(String[] args) {
+        // Create object of subclass
+        Dog d = new Dog();
+        
+        // Child can access both parent and child methods
+        d.eat();   // inherited from Animal class
+        d.bark();  // defined in Dog class
+    }
+ }
+ class Animal {
+ void eat() {
+ System.out.println("Animals can eat");
+ }
+ }
+ class Dog extends Animal {
+ void bark() {
+ System.out.println("Dog can bark");
+ }
+ }
