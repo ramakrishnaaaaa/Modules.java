c61af47772777074515f8aca2becdfee2dcc04ca
ramakrishnaaaaa <ramakrishnaa399@gmail.com>
2025-11-15 22:49:41 +0530

    Create Multiple Inheritance.java

---
+++ Multiple Inheritance.java
@@ -0,0 +1,31 @@
+ // Main class
+ public class MultipleInheritanceExample {
+    public static void main(String[] args) {
+        // Creating object of Dog class
+        Dog d = new Dog();
+        // Calling methods from both interfaces and Dog class
+        d.eat();   // from Animal interface
+        d.play();  // from Pet interface
+        d.bark();  // from Dog class
+    }
+ }
+ // First parent interface
+ interface Animal {
+    void eat();   // method declaration only
+ }
+ // Second parent interface
+ interface Pet {
+    void play();  // method declaration only
+ }
+ // Child class implementing multiple interfaces
+ class Dog implements Animal, Pet {
+ public void eat() {
+ System.out.println("Dog can eat");
+ }
+ public void play() {
+ System.out.println("Dog loves to play");
+ }
+ void bark() {
+ System.out.println("Dog can bark");
+ }
+ }
